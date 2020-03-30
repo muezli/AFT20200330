42ca21c812d4c1831b1834f721c701adce9b2d5c
HertelD-GDF <>
2020-03-30 15:12:04 +0200

    Hozzáadás / Törlés / Rendezés gombok rárakva

--- AFT20200330bevasar/src/bevasarlas/BevasarloGUI.java
+++ AFT20200330bevasar/src/bevasarlas/BevasarloGUI.java
@@ -5,8 +5,11 @@
  */
 package bevasarlas;
 
+import java.awt.BorderLayout;
 import java.awt.Dimension;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 import static javax.swing.WindowConstants.EXIT_ON_CLOSE;
 
 
@@ -17,10 +20,36 @@ public class BevasarloGUI extends JFrame {
     
     public BevasarloGUI() {
         
+        JButton AddItemButton = new JButton("Hozzáadás");
+        JButton RemoveItemButton = new JButton("Törlés");
+        
+        JButton SortAscButton = new JButton("Rendezés (A-Z)");
+        JButton SortDescButton = new JButton("Rendezés (Z-A)");
+             
         setTitle(AppName + " " + VersionNumber);
         setMinimumSize(new Dimension(800, 600));
         setResizable(false);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
+        
+        BorderLayout Layout1 = new BorderLayout();
+        
+        JPanel TopPanel = new JPanel();
+        JPanel CenterPanel = new JPanel();
+        JPanel BottomPanel = new JPanel();
+        
+        this.setLayout(Layout1);
+        this.add(TopPanel, BorderLayout.NORTH);
+        this.add(CenterPanel, BorderLayout.CENTER);
+        this.add(BottomPanel, BorderLayout.SOUTH);
+        
+        
+        
+        
+        BottomPanel.add(AddItemButton);
+        BottomPanel.add(RemoveItemButton);
+        BottomPanel.add(SortAscButton);
+        BottomPanel.add(SortDescButton);
+        
     }
     
 
